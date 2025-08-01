21cde01fe385fe21ee41f059b22ec1c38b174cbb
Vladyslav R <160039489+55kt@users.noreply.github.com>
2025-08-01 21:45:39 +0300

    Initial Commit

---
+++ TwitterApp/TwitterAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterAppApp.swift
+//  TwitterApp
+//
+//  Created by Vlad on 1/8/25.
+//
+
+import SwiftUI
+
+@main
+struct TwitterAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
