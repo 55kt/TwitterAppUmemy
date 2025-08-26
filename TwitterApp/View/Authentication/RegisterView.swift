81df78c345aa6be886191e2c8166e48ed88c71df
Vladyslav R <160039489+55kt@users.noreply.github.com>
2025-08-26 17:06:57 +0300

    Register View & Components

---
+++ TwitterApp/View/Authentication/RegisterView.swift
@@ -0,0 +1,79 @@
+//
+//  RegisterView.swift
+//  TwitterApp
+//
+//  Created by Vlad on 26/8/25.
+//
+
+import SwiftUI
+
+struct RegisterView: View {
+    
+    @State var name = ""
+    @State var email = ""
+    @State var password = ""
+    
+    var body: some View {
+        VStack {
+            ZStack {
+                HStack {
+                    Button {
+                        //
+                    } label: {
+                        Text("Cancel")
+                            .foregroundStyle(.blue)
+                    }
+                    Spacer()
+                }
+                .padding(.horizontal)
+                
+                Image("Twitter")
+                    .resizable()
+                    .scaledToFill()
+                    .padding(.trailing)
+                    .frame(width: 20, height: 20)
+            }
+            
+            Text("Create your account")
+                .font(.title)
+                .bold()
+                .padding(.top, 35)
+            
+            VStack(alignment: .leading, spacing: nil) {
+                CustomAuthTextField(placeholder: "Name", text: $name)
+                CustomAuthTextField(placeholder: "Phone number or Email", text: $email)
+                SecureAuthTextField(placeholder: "Password", text: $password)
+            }
+            
+            Spacer(minLength: 0)
+            
+            VStack {
+                Rectangle()
+                    .frame(height: 1)
+                    .foregroundStyle(.gray)
+                
+                HStack {
+                    Spacer()
+                    
+                    Button {
+                        //
+                    } label: {
+                        Capsule()
+                            .frame(width: 60, height: 30, alignment: .center)
+                            .foregroundStyle(Color(red: 29 / 255, green: 161 / 255, blue: 242 / 255))
+                            .overlay {
+                                Text("Next")
+                                    .foregroundStyle(.white)
+                            }
+                    }
+
+                }
+                .padding(.trailing, 24)
+            }
+        }
+    }
+}
+
+#Preview {
+    RegisterView()
+}
---
+++ TwitterApp/View/Items/SecureAuthTextField.swift
@@ -0,0 +1,37 @@
+//
+//  SecureAuthTextField.swift
+//  TwitterApp
+//
+//  Created by Vlad on 26/8/25.
+//
+
+import SwiftUI
+
+struct SecureAuthTextField: View {
+    var placeholder: String
+    @Binding var text: String
+    
+    var body: some View {
+        VStack {
+            ZStack(alignment: .leading) {
+                if text.isEmpty {
+                    Text(placeholder)
+                        .foregroundStyle(.gray)
+                }
+                
+                SecureField("", text: $text)
+                    .frame(height: 45)
+                    .foregroundStyle(Color(red: 29 / 255, green: 161 / 255, blue: 242 / 255))
+            }
+            Rectangle()
+                .frame(height: 1, alignment: .center)
+                .foregroundStyle(.gray)
+                .padding(.top, -2)
+        }
+        .padding(.horizontal)
+    }
+}
+
+#Preview {
+    SecureAuthTextField(placeholder: "", text: .constant(""))
+}
